33668f1ef2e9afd866e0cc0714eb972eb35f35fb
Antonella G <fuegodjh@gmail.com>
2023-07-30 17:36:12 +0100

    Started Client side e-commerce

---
+++ components/navbar-actions.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const navbarActions = () => {
+  return(
+    <div className="ml-auto flex items-center gap-x-4">
+      hello actions
+    </div>
+  )
+}
+
+export default navbarActions;
