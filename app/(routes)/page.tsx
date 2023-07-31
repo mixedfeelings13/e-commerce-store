bdb50eba54682b7ec935cd5d10c46ae52ddec1f9
Antonella G <fuegodjh@gmail.com>
2023-07-31 04:22:54 +0100

    started billboards components

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,18 @@
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+
+export const revalidate = 0
+
 const HomePage = () => {
   return(
     <div>
-      Hello Store!
+      <Container>
+        <div className="space-y-10 pb-10">
+          <Billboard
+            
+          />
+        </div>
+      </Container>
     </div>
   );
 }
