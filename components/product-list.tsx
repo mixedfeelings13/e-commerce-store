9e90da757c7df5163edde6057dd4e99cc980f512
Antonella G <fuegodjh@gmail.com>
2023-08-06 07:37:23 +0100

    Started product page/related items

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -3,7 +3,6 @@ import { Product } from "@/types";
 import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
 
-
 interface ProductListProps {
   title: string;
   items: Product[]
