ffc84ff09333318bea2c62f5b60b0ab638865231
Antonella G <fuegodjh@gmail.com>
2023-08-06 09:06:21 +0100

    Added filters, product page, gallery

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -2,6 +2,8 @@ import getProduct from "@/actions/get-product"
 import getProducts from "@/actions/get-products"
 import ProductList from "@/components/product-list"
 import Container from "@/components/ui/container"
+import Gallery from "@/components/gallery"
+import Info from "@/components/info"
 
 interface ProductPageProps {
   params: {
@@ -21,11 +23,9 @@ const ProductPage: React.FC<ProductPageProps> = async({
       <Container>
         <div className="px-4 py-10 sm:px-6 lg-px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            {/*Image gallery*/}
-            <div>gallery</div>
+            <Gallery images={product.images}/>
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-              {/*Product info*/}
-              Info
+              <Info data={product}/>
             </div>
           </div>
           <hr className="my-10"/>
