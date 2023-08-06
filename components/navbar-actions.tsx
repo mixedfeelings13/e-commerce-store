6579a29b4d4e61290b6dde23b1a783685cb382f1
Antonella G <fuegodjh@gmail.com>
2023-08-06 10:21:15 +0100

    Cart summary

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -20,7 +20,7 @@ const navbarActions = () => {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button onClick={() => router.push("/cart")} className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag 
           size={20}
           color="white"
