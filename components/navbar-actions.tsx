19e2b3e5deaa75d092b641ab6dd8f67eee9d6891
Antonella G <fuegodjh@gmail.com>
2023-08-07 01:22:26 +0100

    navbar actions update

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,36 +1,39 @@
 "use client";
 
+import { ShoppingBag } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 import Button from "@/components/ui/button";
 import useCart from "@/hooks/use-cart";
-import { ShoppingBag } from "lucide-react";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
 
-const navbarActions = () => {
+const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
-  }, [])
+  }, []);
 
-  const cart = useCart();
   const router = useRouter();
+  const cart = useCart();
 
-  if(!isMounted) return null;
+  if (!isMounted) {
+    return null;
+  }
 
-  return (
+  return ( 
     <div className="ml-auto flex items-center gap-x-4">
-      <Button onClick={() => router.push("/cart")} className="flex items-center rounded-full bg-black px-4 py-2">
-        <ShoppingBag 
+      <Button onClick={() => router.push('/cart')} className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag
           size={20}
           color="white"
         />
         <span className="ml-2 text-sm font-medium text-white">
-          { cart.items.length }
+          {cart.items.length}
         </span>
       </Button>
     </div>
-  )
+  );
 }
-
-export default navbarActions;
+ 
+export default NavbarActions;
